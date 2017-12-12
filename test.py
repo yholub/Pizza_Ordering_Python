0ad63a731fbe43e86244576ba591bb24a8ddc7b1
Andrii Skolotianyi <ask@interlogic.com.ua>
2017-12-12 11:33:54 +0200

    added some example tests

--- test.py
+++ test.py
@@ -2,13 +2,39 @@ import os
 import unittest
 import shutil
 from app import *
- 
+from models import *
+
+TEST_DB = 'test.db'
+
 class BasicTests(unittest.TestCase):
 
     app = __import__("app")
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://(localdb)\\v11.0/Pizza_OrderingTest?driver=SQL+Server+Native+Client+11.0?trusted_connection=yes'
+        print(app.config['SQLALCHEMY_DATABASE_URI'])
+        db = SQLAlchemy(app, metadata=metadata)
+        app.config['db'] = db
+  
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+        ing = Ingredient()
+        ing.Name = "Сир"
+        ing.Price = 10
+        ing.Weight = 5
+        db.session.add(ing)
+        db.session.commit()
 
     def test_main_page(self):
 	    response = self.app.get('/', follow_redirects=True)
+	    self.assertEqual(response.status_code, 200)
+
+    def test_pizza_ordering(self):
+
+	    response = self.app.get('/api/ingredients', follow_redirects=True)
 	    self.assertEqual(response.status_code, 200)
 
 
